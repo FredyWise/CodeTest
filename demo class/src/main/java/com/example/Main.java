ff8b12f41e16512b201c19580bd0352a594ae5a8
Fredy Wise <84549992+FredyWise@users.noreply.github.com>
2024-05-05 02:24:00 +0800

    Add files via upload

---
+++ demo class/src/main/java/com/example/Main.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        bitch b = new bitch("tai");
+        System.out.println("you have " + b.getBabi());
+    }
+}
---
+++ demo class/src/main/java/com/example/bitch.java
@@ -0,0 +1,19 @@
+package com.example;
+public class bitch {
+    private String babi;
+
+    public bitch(String babi){
+        // this.babi=babi;
+        this.setBabi(babi);
+    }
+
+    public void setBabi(String babi){
+        this.babi = babi;
+    }
+
+    public String getBabi(){
+        return this.babi;
+    }
+
+}
+
